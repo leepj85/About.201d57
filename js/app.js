589f0ccf35c6fbd31ea19e4481c08f29dfd127b2
Peter Lee <leepj85@gmail.com>
2019-06-06 07:22:04 -0700

    odified code for Lab3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-alert('Let\'s play a guessing game! Try to determine what my 5 responses (Yes/No) would be.');
+alert('Let\'s play a guessing game! Try to determine what my 7 responses (Yes/No) would be.');
 
 //Prompt user's name.
 var userName = prompt('How may I address you?');
@@ -8,6 +8,8 @@ alert('And so it begins ' + userName + '...');
 
 //Variables to hold user input from prompt. Then check input and respond to user.
 var countCorrect = 0; 
+
+//Question 1
 var inputMovie = prompt('Do you think I enjoy movies?').toLowerCase();
 if (inputMovie === 'yes' || inputMovie === 'y') {
   alert('You are right - movies are a huge part of my life!');
@@ -17,7 +19,7 @@ if (inputMovie === 'yes' || inputMovie === 'y') {
 } else {
   alert('What kind of answer is that?!!??!');
 }
-
+//Question 2
 var inputOutdoors = prompt('Is the outdoors greatness to me?').toLowerCase();
 if (inputOutdoors === 'yes' || inputOutdoors === 'y') {
   alert('There is something so calming about Nature, I thoroughly enjoy the outdoors.');
@@ -28,6 +30,7 @@ if (inputOutdoors === 'yes' || inputOutdoors === 'y') {
   alert('What kind of answer is that?!!??!');
 }
 
+//Question 3
 var inputCode = prompt('Have I coded previously and/or am I a compentent developer?').toLowerCase();
 if (inputCode === 'yes' || inputCode === 'y') {
   alert('Indeed.');
@@ -38,6 +41,7 @@ if (inputCode === 'yes' || inputCode === 'y') {
   alert('What kind of answer is that?!!??!');
 }
 
+//Question 4
 var inputPowers = prompt('Do I have any secret super powers?').toLowerCase();
 if (inputPowers === 'yes' || inputPowers === 'y') {
   alert('Awwwwww you think so highly of me *blush*');
@@ -48,6 +52,7 @@ if (inputPowers === 'yes' || inputPowers === 'y') {
   alert('What kind of answer is that?!!??!');
 }
 
+//Question 5
 var inputSeafood = prompt('Am I a seafood lover?').toLowerCase();
 if (inputSeafood === 'yes' || inputSeafood === 'y') {
   alert('nom nom nom nom nom!');
@@ -57,6 +62,60 @@ if (inputSeafood === 'yes' || inputSeafood === 'y') {
 } else {
   alert('What kind of answer is that?!!??!');
 }
+
+
+//Question 6
+var inputNumGuess = prompt('Guess a number between 1 to 10. (you have 4 tries!');
+var myNum = '3';
+
+for (var i = 3; i > 0; i--) {
+  if (inputNumGuess === myNum) {
+    alert('You are correctomondo!');
+    countCorrect++;
+    break;
+  } else if (inputNumGuess < myNum) {
+    inputNumGuess = prompt('Wrong, your guess is too low. Guess again...'); 
+  } else if (inputNumGuess > myNum) {
+    inputNumGuess = prompt('Wrong, your guess is too high. Guess again...');
+  }
+}
+/*
+//, 'green bell peppers', 'italian sauage', 'onions', 'tomatoes'
+//Question 7
+var answerBank = ['pepperoni'];
+var inputPizzaTop = prompt('Can you guess my favoriate pizza toppings? You have 6 tries to get 1 right!').toLowerCase();
+var correctAnswer = false;
+
+
+//User has 6 attempts to get correct answer.
+for (var j = 0; j < 6; j++) {
+  //Check to see if users input is in my array.
+  for (var p = 0; p < answerBank.length; p++) {
+    if (inputPizzaTop === answerBank[p]) {
+      alert('That\'s correct! Here are all my favoriate toppings: ' + answerBank);
+      countCorrect++;
+      j = 'null'; //break out of outer loop.
+      break;
+    } else if (p === answerBank.length-1){
+      alert('Sorry that is not one of my favorite toppings');
+      break;
+    }
+  }
+}
+
+//2nd try at question 7 
+do {
+  //check if answer is correct
+  for (var p = 0; p < answerBank.length; p++) {
+    if (inputPizzaTop === answerBank[p]) {
+      alert('That\'s correct! Here are all my favoriate toppings: ' + answerBank);
+      correctAnswer = true;
+    }
+}
+while(!correctAnswer); 
+*/
+
+
 
 //Send ending message to user with results of guessing game.
 alert('Thanks for playing ' + userName + '. You got ' + countCorrect + '\\5 correct.');
@@ -69,3 +128,5 @@ console.log('inputCode: ' + inputCode);
 console.log('inputPowers: ' + inputPowers);
 console.log('inputSeafood: ' + inputSeafood);
 console.log('countCorrect: ' + countCorrect);
+console.log('inputNumGuess: ' + inputNumGuess);
+console.log(answerBank);
